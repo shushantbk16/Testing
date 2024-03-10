054404e8833df6257b340466d98de2e3f78ec1a8
shushantbk16 <shushantkamatar29@gmail.com>
2024-03-10 12:39:13 +0530

    checking

--- types.js
+++ types.js
@@ -7,7 +7,7 @@ const createTodo = zod.object({
 const updateTodo = zod.object({
   id: zod.string(),
 });
-console.log(createTodo);
+console.log("hello");
 module.exports = {
   createTodo: createTodo,
   updateTodo: updateTodo,
